eb87a81b0fb6c3d776ac954a8502a0604e6de563
vvitoss <orangekaramel@gmail.com>
2022-01-20 15:13:10 +0300

    fixed syntax

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -6,9 +6,9 @@ class SQRServiceTest {
 
     @org.junit.jupiter.api.Test
     void countingNumSquare() {
-        SQRService SQRService = new SQRService();
+        SQRService service = new SQRService();
         int expected = 3;
-        int actual = SQRService.countingNumSquare(200 , 300 );
-        assertEquals(expected ,actual);
+        int actual = service.countingNumSquare(200, 300);
+        assertEquals(expected, actual);
     }
 }
